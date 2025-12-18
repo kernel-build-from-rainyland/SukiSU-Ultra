ebe964c230c29a1f482fe9fe0fda0ec5da7fb1a7
RainyXeon <rainyxeon@gmail.com>
2025-12-18 13:04:11 +0700

    add: kowsu and ksun hash

--- kernel/manager_sign.h
+++ kernel/manager_sign.h
@@ -33,6 +33,14 @@
 #define EXPECTED_HASH_NEKO                                                     \
 	"946b0557e450a6430a0ba6b6bccee5bc12953ec8735d55e26139b0ec12303b21"
 
+// KOWX712/KernelSU
+#define EXPECTED_SIZE_KOWX712 0x375
+#define EXPECTED_HASH_KOWX712 "484fcba6e6c43b1fb09700633bf2fb4758f13cb0b2f4457b80d075084b26c588"
+
+// KernelSU-Next/KernelSU-Next
+#define EXPECTED_SIZE_NEXT 0x3e6
+#define EXPECTED_HASH_NEXT "79e590113c4c4c0c222978e413a5faa801666957b1212a328e46c00c69821bf7"
+
 // Dynamic Sign
 #define EXPECTED_SIZE_OTHER 0x300
 #define EXPECTED_HASH_OTHER                                                    \
